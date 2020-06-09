cd9264f563693a20b769ccddbf027696020385f4
Nevena <nvndjukin97@gmail.com>
2020-06-09 23:51:51 +0200

    Added cert generator

--- bsep/src/main/java/bsep/model/SubjectData.java
+++ bsep/src/main/java/bsep/model/SubjectData.java
@@ -4,7 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import sun.security.x509.X500Name;
+import org.bouncycastle.asn1.x500.X500Name;
+
 
 import java.security.PublicKey;
 import java.util.Date;
